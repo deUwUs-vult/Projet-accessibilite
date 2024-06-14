408ac8e39e6cf2d1928e91689281b7a4ea8d1be2
vardose <giraudonaxel@gmail.com>
2024-06-14 10:47:00 +0200

    v0.1 of the component for the Quiz name

--- src/components/nameForQuiz.tsx
+++ src/components/nameForQuiz.tsx
@@ -7,7 +7,7 @@ export default function NameForQuiz() {
             <div className="flex flex-col self-center items-center justify-center w-1/2 h-full">
                 <h1 className="text-white text-4xl font-bold">What's the name of the Quiz ?</h1>
                 <input type="text" className="w-1/2 h-10  mt-5 p-2 bg-transparent focus: outline-none" placeholder="Entrez le nom du quiz"/>
-                <button className="mt-5 bg-[#FFFFFF] self-end text-black w-fit px-2 h-10 rounded-lg">Next</button>
+                <button className="mt-5 bg-[#FFFFFF] self-end text-black w-fit px-2 h-10 rounded-lg" type={"submit"}>Next</button>
             </div>
             <Footer></Footer>
         </div>
