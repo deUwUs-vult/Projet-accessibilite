0ce26ebb06d246c93f2a771cee4fd5ac711b253a
vardose <giraudonaxel@gmail.com>
2024-06-14 10:35:31 +0200

    modified templates.tsx component

--- src/components/templates.tsx
+++ src/components/templates.tsx
@@ -1,14 +1,24 @@
 "use client";
+import Link from 'next/link';
 import React, { useEffect } from 'react';
 
 type Props = {
     id: number;
+    name: string;
 };
 
 export default function Template(props: Props) {
     return (
-        <div className="cursor-pointer m-4 bg-buttonBg text-center px-4 py-2 rounded" onClick={() => {window.location.assign(`/template/${props.id}`)}}>
-        <p>Choose me : {props.id}</p>
-    </div>
+        <Link href={`/template/${props.id}`} className=" bg-[#777777] max-w-sm rounded overflow-hidden shadow-lg m-4">
+            {/* <img className="w-full" src="" alt=""> </img> */}
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{props.name}</div>
+                <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+            </div>
+    </Link>
     );
 }
