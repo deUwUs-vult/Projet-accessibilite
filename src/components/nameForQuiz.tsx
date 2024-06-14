848ad0554101ce5e545b735ed721951470fb9b78
vardose <giraudonaxel@gmail.com>
2024-06-14 10:45:05 +0200

    v0.1 of the component for the Quiz name

---
+++ src/components/nameForQuiz.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./footer";
+
+export default function NameForQuiz() {
+    return (
+        <div className='bg-[#383F51] w-screen h-screen flex flex-col'>
+            <div className="flex flex-col self-center items-center justify-center w-1/2 h-full">
+                <h1 className="text-white text-4xl font-bold">What's the name of the Quiz ?</h1>
+                <input type="text" className="w-1/2 h-10  mt-5 p-2 bg-transparent focus: outline-none" placeholder="Entrez le nom du quiz"/>
+                <button className="mt-5 bg-[#FFFFFF] self-end text-black w-fit px-2 h-10 rounded-lg">Next</button>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+}
